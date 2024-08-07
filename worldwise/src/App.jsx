56d3bc2da51b8ddc27dd0d36adf3d7089292e7ba
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-08-07 16:41:18 +0300

    started work on the router

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,12 +1,21 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  const x = 0;
+
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
