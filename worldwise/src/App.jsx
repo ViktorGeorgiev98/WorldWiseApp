6df80c953ef45a1c2a90395b5f1794719dcab94b
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-08-10 14:02:23 +0300

    created fake api with json server

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -6,6 +6,7 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
 
@@ -16,8 +17,8 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
