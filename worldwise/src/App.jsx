5a38917ceda12501cbed48126ce9fd265cc768b0
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-08-11 14:04:22 +0300

    programatic navigation (going back button, navigate after submitting form)

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -9,6 +9,8 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Form from "./components/Form";
 const URL = "http://localhost:8000";
 
 function App() {
@@ -40,10 +42,11 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route index element={<Navigate replace to='cities' />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,4 +1,6 @@
+import { useParams, useSearchParams } from "react-router-dom";
 import styles from "./City.module.css";
+
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -9,19 +11,19 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function City() {
-  // TEMP DATA
-  const currentCity = {
-    cityName: "Lisbon",
-    emoji: "🇵🇹",
-    date: "2027-10-31T15:59:59.138Z",
-    notes: "My favorite city so far!",
-  };
+  const { id } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+    const lat = searchParams.get('lat');
+    const lng = searchParams.get('lng');
 
-  const { cityName, emoji, date, notes } = currentCity;
+
+
+  // const { cityName, emoji, date, notes } = currentCity;
 
   return (
     <div className={styles.city}>
-      <div className={styles.row}>
+      <h1>Position: {lng}, {lat}</h1>
+      {/* <div className={styles.row}>
         <h6>City name</h6>
         <h3>
           <span>{emoji}</span> {cityName}
@@ -53,7 +55,7 @@ function City() {
 
       <div>
         <ButtonBack />
-      </div>
+      </div> */}
     </div>
   );
 }
