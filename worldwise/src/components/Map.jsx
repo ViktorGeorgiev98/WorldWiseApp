85ddfa7b27ca28df086ed31b4f36e81ef0c0ee35
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-08-21 15:53:20 +0300

    marker for each city in the db

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -2,6 +2,7 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import { useState } from 'react';
+import { useCities } from '../contexts/CitiesContext';
 
 export default function Map() {
     const navigate = useNavigate();
@@ -9,6 +10,7 @@ export default function Map() {
     const [searchParams, setSearchParams] = useSearchParams();
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
+    const { cities } = useCities();
 
     return (
         <div className={styles.mapContainer}>
@@ -18,14 +20,17 @@ export default function Map() {
                 zoom={13} 
                 scrollWheelZoom={true}>
                 <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.fr/hot">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.fr/hot">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={mapPosition}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-                </Marker>
+                {cities.map((city) => (
+                    <Marker position={[city.position.lat, city.position.lng]} key={city.id}>
+                        <Popup>
+                            <span>{city.emoji}</span>
+                            <span>{city.cityName}</span>
+                        </Popup>
+                    </Marker>
+                ))}
             </MapContainer>
         </div>
     )
