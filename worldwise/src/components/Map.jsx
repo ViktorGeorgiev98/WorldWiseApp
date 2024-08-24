e027e08c574e0f1e9d3c5f571edff9f8233c9622
Viktor Georgiev <122877168+ViktorGeorgiev98@users.noreply.github.com>
2024-08-24 14:49:07 +0300

    used geolocation to implement current position

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -3,22 +3,36 @@ import styles from './Map.module.css';
 import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvent } from 'react-leaflet';
 import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
+import { useGeolocation } from '../hooks/useGeolocation';
+import  Button from '../components/Button';
 
 export default function Map() {
     const [mapPosition, setMapPosition] = useState([40, 0]);
     const [searchParams, setSearchParams] = useSearchParams();
+    const { isLoading: isLoadingPosition, position: geolocationPosition, getPosition } = useGeolocation();
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
     const { cities } = useCities();
+
+    useEffect(function() {
+        if (geolocationPosition) {
+            setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+        };
+    }, [geolocationPosition]);
 
     useEffect(() => {
         if (lat && lng) {
             setMapPosition([lat, lng]);
         }
-    }, [lat, lng])
+    }, [lat, lng]);
 
     return (
         <div className={styles.mapContainer}>
+            {!geolocationPosition &&  <Button 
+                    type="position" 
+                    onClick={getPosition}>
+                    {isLoadingPosition ? "Is Loading position..." : "Use your position"}
+                </Button>}
             <MapContainer 
                 className={styles.map} 
                 center={mapPosition} 
